cbde679098cedb054091c6a0a30acc585c941c9b
aldu <aldu@debian.9.4.0.vbx>
2018-05-14 01:09:23 +0200

    replace deprec @esm by esm

--- download.sh
+++ download.sh
@@ -1,5 +1,6 @@
-BASE_PATH=/Users/albertdupre/Documents/node.js/bookdownload
+BASE_PATH=~/scripts/bookdownload
 export NODE_PATH=$NODE_PATH:$BASE_PATH/node_modules
+
 if [ -n $1 ]
 then
     node -r @std/esm $BASE_PATH/main.mjs $* 
